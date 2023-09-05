290b7d40787ebef524a140323d9552b28bb9ce1c
AndreaBassani <bassani.andrea@yahoo.com>
2023-09-05 18:48:55 +0100

    adding alternative cloustering method, bolt question text, add centroid value

--- DS_In_5_years_survey.py
+++ DS_In_5_years_survey.py
@@ -5,16 +5,19 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 from sklearn.cluster import KMeans
+from k_means_constrained import KMeansConstrained
 from sklearn.preprocessing import StandardScaler
 from scipy.spatial.distance import euclidean
 
 with st.form('user_input'):
     st.write('Fill in the form')
-    name = st.text_input('Name')
+    name = st.text_input('Name', placeholder='')
+    
+    st.markdown('---')
     
     st.markdown(
                 """
-        How AI will impact the job market in the next 5 years
+        **How AI will impact the job market in the next 5 years**
         - 0: AI will have a negative impact on the job market, reducing the number of jobs
         - 50: AI will have a neutral impact on the job market
         - 100: AI will have a positive impact on the job market, creating new jobs
@@ -22,9 +25,11 @@ with st.form('user_input'):
     )
     ds_jobs = st.slider("Changes in job market", 0, 100)
     
+    st.markdown('---')
+    
     st.markdown(
                 """
-        How AI will impact the Data Science and Data Engineering skills in the next 5 years
+        **How AI will impact the Data Science and Data Engineering skills in the next 5 years**
         - 0: AI will not have any impact on the Data Science and Data Engineering skills
         - 50: 50% of the Data Science and Data Engineering skills will be impacted by AI (prompt engineering, code generation, etc.)
         - 100: 100% of the Data Science and Data Engineering skills will be impacted by AI
@@ -37,8 +42,8 @@ with st.form('user_input'):
     'DS_jobs_impact':ds_jobs,
     'DS_skills_impact':ds_skills_impact,
     }
-
-    user_input_submit_button = st.form_submit_button("Submit")
+  
+    user_input_submit_button = st.form_submit_button("Submit", disabled= not name)
 
 if os.path.exists("data/user_input_collection.csv"):
     users_input_collection = pd.read_csv("data/user_input_collection.csv")
@@ -94,76 +99,81 @@ if 'users_input_collection' in locals():
         if len(users_input_collection) >= 4:
             # 1. Load and preprocess data
             df = users_input_collection.copy()
+            df = df.sort_values(by=['DS_skills_impact', 'DS_jobs_impact'], ascending=True).reset_index(drop=True)
+                                    
+            clustering_model = st.toggle('Activate proprietary clustering algorithm')  
+                                       
+            if clustering_model:
 
-            # Normalize the features
-            scaler = StandardScaler()
-            df[['DS_skills_impact', 'DS_jobs_impact']] = scaler.fit_transform(df[['DS_skills_impact', 'DS_jobs_impact']])
+                # Normalize the features
+                scaler = StandardScaler()
+                df[['DS_skills_impact', 'DS_jobs_impact']] = scaler.fit_transform(df[['DS_skills_impact', 'DS_jobs_impact']])
 
-            # Get cluster assignments using KMeans
-            number_of_clusters = 4
-            kmeans = KMeans(n_clusters=number_of_clusters)
-            df['cluster'] = kmeans.fit_predict(df[['DS_skills_impact', 'DS_jobs_impact']])
-            centroids = kmeans.cluster_centers_
+                # Get cluster assignments using KMeans
+                number_of_clusters = 4
+                kmeans = KMeans(n_clusters=number_of_clusters)
+                df['cluster'] = kmeans.fit_predict(df[['DS_skills_impact', 'DS_jobs_impact']])
+                centroids = kmeans.cluster_centers_
 
-            # Determine ideal cluster size and current cluster sizes
-            ideal_size = len(df) // number_of_clusters
-            cluster_sizes = df['cluster'].value_counts().to_dict()
+                # Determine ideal cluster size and current cluster sizes
+                ideal_size = len(df) // number_of_clusters
+                cluster_sizes = df['cluster'].value_counts().to_dict()
 
-            # Create a mapping of clusters to points
-            cluster_points = {i: df[df['cluster'] == i].index.tolist() for i in range(number_of_clusters)}
+                # Create a mapping of clusters to points
+                cluster_points = {i: df[df['cluster'] == i].index.tolist() for i in range(number_of_clusters)}
 
-            # For clusters that are too big, move some of their points to clusters that are too small
-            for cluster, size in cluster_sizes.items():
-                while size > ideal_size:
-                    # Find the closest other cluster that's too small
-                    current_centroid = centroids[cluster]
-                    distances_to_other_centroids = [(euclidean(current_centroid, centroids[i]), i) for i in range(number_of_clusters) if cluster_sizes[i] < ideal_size]
-                    if not distances_to_other_centroids:
-                        break
-                    closest_cluster = min(distances_to_other_centroids, key=lambda x: x[0])[1]
-                    
-                    # Move the furthest point from this cluster to the closest cluster
-                    points_in_current_cluster = cluster_points[cluster]
-                    distances_to_current_centroid = [(euclidean(df.loc[p][['DS_skills_impact', 'DS_jobs_impact']].values, current_centroid), p) for p in points_in_current_cluster]
-                    furthest_point = max(distances_to_current_centroid, key=lambda x: x[0])[1]
-                    df.loc[furthest_point, 'cluster'] = closest_cluster
-                    cluster_points[closest_cluster].append(furthest_point)
-                    cluster_points[cluster].remove(furthest_point)
-                    
-                    # Update cluster sizes
-                    cluster_sizes[cluster] -= 1
-                    cluster_sizes[closest_cluster] += 1
-                    size -= 1
+                # For clusters that are too big, move some of their points to clusters that are too small
+                for cluster, size in cluster_sizes.items():
+                    while size > ideal_size + 1:
+                        # Find the closest other cluster that's too small
+                        current_centroid = centroids[cluster]
+                        distances_to_other_centroids = [(euclidean(current_centroid, centroids[i]), i) for i in range(number_of_clusters) if cluster_sizes[i] <= ideal_size]
+                        if not distances_to_other_centroids:
+                            break
+                        closest_cluster = min(distances_to_other_centroids, key=lambda x: x[0])[1]
+                        
+                        # Move the furthest point from this cluster to the closest cluster
+                        points_in_current_cluster = cluster_points[cluster]
+                        distances_to_current_centroid = [(euclidean(df.loc[p][['DS_skills_impact', 'DS_jobs_impact']].values, current_centroid), p) for p in points_in_current_cluster]
+                        furthest_point = max(distances_to_current_centroid, key=lambda x: x[0])[1]
+                        df.loc[furthest_point, 'cluster'] = closest_cluster
+                        cluster_points[closest_cluster].append(furthest_point)
+                        cluster_points[cluster].remove(furthest_point)
+                        
+                        # Update cluster sizes
+                        cluster_sizes[cluster] -= 1
+                        cluster_sizes[closest_cluster] += 1
+                        size -= 1
+                        
+                # Inverse transform the normalized features back to their original scales
+                df[['DS_skills_impact', 'DS_jobs_impact']] = scaler.inverse_transform(df[['DS_skills_impact', 'DS_jobs_impact']])
+
+                # Inverse transform the centroids
+                original_centroids = scaler.inverse_transform(centroids)
+            
+            else:
+                clf = KMeansConstrained(
+                        n_clusters=4,
+                        size_min=len(users_input_collection)//4,
+                        size_max=(len(users_input_collection)//4)+1,
+                        random_state=0
+            )
+                df['cluster'] = clf.fit_predict(df[['DS_skills_impact', 'DS_jobs_impact']])
+                original_centroids = clf.cluster_centers_
             
             # Create a helper 'rank' column that enumerates each entry within its cluster
             df['rank'] = df.groupby('cluster').cumcount()
-
             # Pivot the dataframe
             pivoted_df = df.pivot(index='rank', columns='cluster', values='name')
 
             # Rename columns for clarity
-            pivoted_df.columns = [f'Group_{col}' for col in pivoted_df.columns]
+            pivoted_df.columns = [f'Group_{col + 1}' for col in pivoted_df.columns]
             
-            st.dataframe(pivoted_df)
+            st.dataframe(pivoted_df, use_container_width=True, hide_index=True)
             
-            # Inverse transform the normalized features back to their original scales
-            df[['DS_skills_impact', 'DS_jobs_impact']] = scaler.inverse_transform(df[['DS_skills_impact', 'DS_jobs_impact']])
+            centroids_df = pd.DataFrame(original_centroids, columns=['DS_skills_impact', 'DS_jobs_impact'], index = pivoted_df.columns).transpose()
+            st.dataframe(centroids_df.loc[::-1, :], use_container_width=True)
 
-            # Inverse transform the centroids
-            original_centroids = scaler.inverse_transform(centroids)
-            
-            # # Visualization
-            # fig, ax = plt.subplots()
-            # ax.scatter(df['DS_jobs_impact'], df['DS_skills_impact'], c=df['cluster'], cmap='rainbow')
-            # ax.scatter(original_centroids[:, 1], original_centroids[:, 0], s=200, marker='X', c='black')
-            # ax.set_xlabel('DS_job')
-            # ax.set_ylabel('DS_skills_impact')
-            # ax.set_title('Clusters based on people preferences')
-            # # Set the background color of the figure and axes to transparent
-            # fig.patch.set_facecolor('none')
-            # ax.patch.set_facecolor('none')
-            # st.pyplot(fig)
-            
             # Visualization using Plotly
             trace_points = go.Scatter(x=df['DS_jobs_impact'], y=df['DS_skills_impact'], 
                                     mode='markers', 
